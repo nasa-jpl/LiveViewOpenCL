11a7f009cd5fd20c140310488decf76938dce4d2
Maxwell Plotkin <maxwell.a.plotkin@jpl.nasa.gov>
2019-01-23 14:51:07 -0800

    #11 started install script

---
+++ edt_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#IFS='.' read -a VERS <<< "2.6.18"
+IFS='.' read -a VERS <<< "$(uname -r)"
+
+if [ ${VERS[0]} -lt 2 -o ${VERS[0]} -gt 4 ] \
+|| [ ${VERS[0]} -eq 2 -a ${VERS[1]} -lt 6 ] \
+|| [ ${VERS[0]} -eq 2 -a ${VERS[1]} -eq 6 -a ${VERS[2]} -le 18 ] \
+|| [ ${VERS[0]} -eq 4 -a ${VERS[1]} -gt 14 ]; then
+    echo "Script requires Linux Kernel version 2.6.19..4.15.x";
+else
+    if [ -d "/opt/EDTpdv/" ]; then
+        if [ -x "/opt/EDTpdv/uninstall.sh" ]; then
+            echo "Uninstalling existing EDTpdv files.";
+            #/opt/EDTpdv/uninstall.sh
+        else
+            echo "/opt/EDTpdv exists; failed to run EDT uninstall script."
+        fi
+    else
+        echo "Downloading EDTpdv drivers..."
+        #wget etc
+    fi
+fi
