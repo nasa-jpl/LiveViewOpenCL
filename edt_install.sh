6b899240a251e553eb2d0ed2f4f6cfbd055e077a
Maxwell Plotkin <maxwell.a.plotkin@jpl.nasa.gov>
2019-01-25 12:21:30 -0800

    #11 a couple more edt_install bugfixes

--- edt_install.sh
+++ edt_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-IFS='.' read -a VERS <<< "$(uname -r)"
-EDTDIR="/opt/EDTpdv/"
+IFS='-' read -a PREVERS <<< "$(uname -r)"
+IFS='.' read -a VERS <<< "${PREVERS[0]}"
+EDTDIR="/opt/EDTpdv"
 
 if [ ${VERS[0]} -lt 2 -o ${VERS[0]} -gt 4 ] \
 || [ ${VERS[0]} -eq 2 -a ${VERS[1]} -lt 6 ] \
@@ -8,10 +9,10 @@ if [ ${VERS[0]} -lt 2 -o ${VERS[0]} -gt 4 ] \
 || [ ${VERS[0]} -eq 4 -a ${VERS[1]} -gt 15 ]; then
     echo "Script requires Linux Kernel version 2.6.19..4.15.x"
 else
-    if [ -d "$EDTDIR "]; then
+    if [ -d "$EDTDIR" ]; then
         if [ -x "$EDTDIR/uninstall.sh" ]; then
             echo "Uninstalling existing EDTpdv files."
-            pushd "$EDTDIR/uninstall.sh" && ./uninstall.sh
+            pushd "$EDTDIR" && ./uninstall.sh && rmdir "$EDTDIR"
             popd
         else
             echo "$EDTDIR exists; failed to run EDT uninstall script."
@@ -22,6 +23,9 @@ else
     echo "Downloading EDTpdv drivers..."
     wget https://edt.com/downloads/pdv_5-5-5-8_run/ -O EDTpdv.run
     echo "Installing EDTpdv drivers..."
-    chmod +x ./EDTpdv.run && ./EDTpdv.run "$EDTDIR" && \
-    mkdir ./EDT_include && cp "$EDTDIR/*h" ./EDT_include
+    chmod +x ./EDTpdv.run && ./EDTpdv.run "$EDTDIR"
+    if [! -d ./EDT_include ]; then
+        mkdir ./EDT_include
+    fi
+    cp "$EDTDIR/*h" ./EDT_include
 fi
