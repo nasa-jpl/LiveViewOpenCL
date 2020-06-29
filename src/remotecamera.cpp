5ab7a86c248a754d5e4f46759d1a75fce963e687
scott-huson <randomrandom623@gmail.com>
2020-06-29 17:18:59 -0400

    Fixed git branch location issue

--- src/remotecamera.cpp
+++ src/remotecamera.cpp
@@ -49,137 +49,30 @@ RemoteCamera::~RemoteCamera()
     readLoopFuture.waitForFinished();
 }
 
-//std::string RemoteCamera::getFname()
-//{
-//    std::string fname; // will return empty string if no unread files are found.
-//    std::vector<std::string> fname_list;
-//    bool has_file = false;
-//    if (data_dir.empty()) {
-//        return fname;
-//    }
-//    if (image_no < xio_files.size()) {
-//        fname = xio_files[image_no++];
-//    } else {
-//        os::listdir(fname_list, data_dir);
-//        if (fname_list.size() < 1) {
-//            return fname;
-//        }
-//        /* if necessary, there may need to be code to sort the "frames" in the data directory
-//        * by product name, as mtime is unreliable.
-//        */
-//        std::sort(fname_list.begin(), fname_list.end(), doj::alphanum_less<std::string>());
-//        for (auto f = fname_list.end() - 1; f != fname_list.begin(); --f) {
-//            has_file = std::find(xio_files.begin(), xio_files.end(), *f) != xio_files.end();
-//            std::string ext = os::getext(*f);
-//            std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
-//            if ((*f).empty() or (std::strcmp(ext.data(), "xio") != 0 and
-//                                 std::strcmp(ext.data(), "decomp") != 0)) {
-//                continue;
-//            } else if (has_file) {
-//                break;
-//            } else {
-//                xio_files.emplace_back(*f);
-//            }
-
-//        }
-
-//        if (image_no < xio_files.size()) {
-//            fname = xio_files[image_no++];
-//        }
-//    }
-
-//    return fname;
-//}
-
-//void RemoteCamera::readFile()
-//{
-//    is_reading = true;
-//    bool validFile = false;
-//    while(!validFile) {
-//        ifname = getFname();
-//        if (ifname.empty()) {
-//            if (dev_p.is_open()) {
-//                dev_p.close();
-//            }
-
-//            if (running.load()) {
-//                running.store(false);
-//                emit timeout();
-//            }
-//            return; //If we're out of files, give up
-//        }
-//        // otherwise check if data is valid
-//        dev_p.open(ifname, std::ios::in | std::ios::binary);
-//        if (!dev_p.is_open()) {
-//            qDebug() << "Could not open file" << ifname.data() << ". Does it exist?";
-//            dev_p.clear();
-//            readFile();
-//            return;
-//        }
-
-//        // qDebug() << "Successfully opened " << ifname.data();
-//        dev_p.unsetf(std::ios::skipws);
-
-//        dev_p.read(reinterpret_cast<char*>(header.data()), headsize);
-
-//        std::streampos filesize(0);
-//        std::string ext = os::getext(ifname);
-//        std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
-//        if (!std::strcmp(ext.data(), "decomp")) {
-//            dev_p.seekg(0, std::ios::end);
-//            filesize = dev_p.tellg();
-//            dev_p.seekg(headsize, std::ios::beg);
-//        } else {
-//            // convert the raw hex string to decimal, one digit at a time.
-//            filesize = int(header[7]) * 16777216 + int(header[6]) * 65536 + int(header[5]) * 256 + int(header[4]);
-//        }
-
-//        framesize = static_cast<size_t>(filesize / nFrames);
-//        if (framesize == 0) { //If header reports a 0 filesize (invalid data), then skip this file.
-//            dev_p.close();
-//            qDebug().nospace() << "Skipped file \"" << ifname.data() << "\" due to invalid data.";
-//        } else { //otherwise we load it
-//            validFile = true;
-//            dev_p.seekg(headsize, std::ios::beg);
-
-//            // qDebug() << "File size is" << filesize << "bytes, which corresponds to a framesize of" << framesize << "bytes.";
-
-//            std::vector<uint16_t> zero_vec(size_t(frame_width * data_height) - (size_t(framesize) / sizeof(uint16_t)));
-//            std::fill(zero_vec.begin(), zero_vec.end(), 0);
-
-//            std::vector<uint16_t> copy_vec(size_t(framesize), 0);
-
-//            for (int n = 0; n < nFrames; ++n) {
-//                dev_p.read(reinterpret_cast<char*>(copy_vec.data()), std::streamsize(framesize));
-//                frame_buf.emplace_front(copy_vec);
-
-//                if (framesize / sizeof(uint16_t) < size_t(frame_width * data_height)) {
-//                    std::copy(zero_vec.begin(), zero_vec.end(), frame_buf[size_t(n)].begin() + framesize / sizeof(uint16_t));
-//                }
-//            }
-
-//            running.store(true);
-//            emit started();
-//            dev_p.close();
-//        }
-//    }
-//}
-
-//void RemoteCamera::readLoop()
-//{
-//    QTime remTime;
-//    do {
-//        // Yeah yeah whatever it's a magic buffer size recommendation
-//        if (frame_buf.size() <= 96) {
-//            readFile();
-//        } else {
-//            remTime = QTime::currentTime().addMSecs(tmoutPeriod);
-//            while(QTime::currentTime() < remTime) {
-//                QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
-//            }
-//        }
-//    } while (is_reading);
-//}
+void RemoteCamera::SocketRead()
+{
+    uint32_t byte_pos = 0; // Two bytes per pixel
+    uint32_t frame_byte_size = framesize*2;
+    do {
+        if (!socket->waitForReadyRead(2000)) // If it timed out
+        {
+            qDebug() << "Timed out" << byte_pos;
+            is_receiving = false;
+            break; // Return existing frame if we wait too long
+        }
+        // Convert the data
+        QByteArray buffer = socket->read(frame_byte_size - byte_pos);
+        size_t dataSize = buffer.size();
+        QDataStream dstream(buffer);
+        for (uint32_t i = byte_pos; i < dataSize + byte_pos; i++) // Go through each byte in the message
+        {
+            uint16_t temp_int;
+            dstream >> temp_int;
+            temp_frame[i] = (temp_int >> 8) | ( temp_int << 8); // Bits are interpretted as mid-little endian, so we just shift them back
+        }
+        byte_pos += dataSize;
+    } while (byte_pos < frame_byte_size);
+}
 
 uint16_t* RemoteCamera::getFrame()
 {
